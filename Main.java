cbbc8a81c6f7fa8827cabd573b50349b7ca63a69
Michael DiRamio <>
2021-05-27 18:00:14 +0000

    Initial commit with graphics library

---
+++ Main.java
@@ -0,0 +1,13 @@
+import DLibX.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Testing that DConsole works
+    DConsole dc = new DConsole(600, 400);
+    dc.fillRect(200, 100, 90, 180);
+    dc.redraw();
+
+  }
+
+}
