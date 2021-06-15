be56ac16996902b2014b146cdad84a87baecf0f5
mccreag <75863164+Halcionic@users.noreply.github.com>
2021-06-15 17:35:49 +0000

    Added Masons and Joes minigames

--- Main.java
+++ Main.java
@@ -18,9 +18,9 @@ public class Main {
     ArrayList<MiniGame> game = new ArrayList<>();
     //add games here vvvvv
     //basic format game.add(new ______());
-    //game.add(new FlappyBird());
+    game.add(new FlappyBird());
     game.add(new PerfectTiming());
-    //game.add(new UpOrDown());
+    game.add(new UpOrDown());
 
     //sorting MiniGames
     ArrayList<MiniGame> games = new ArrayList<>();
